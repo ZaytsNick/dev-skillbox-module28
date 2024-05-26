b0c3cb4670fa88a74fcf354cb412c86ed7920fef
zayts_home <10kcin01@gmail.com>
2024-05-26 17:44:34 +0500

    Task#1 remake

--- Task#1/main1.cpp
+++ Task#1/main1.cpp
@@ -5,7 +5,7 @@
 #include <map>
 #include <mutex>
 
-std::map<int, std::vector<std::string>> top;
+std::vector<std::string> top;
 std::mutex topList;
 
 void swim(int speed, std::string name)
@@ -20,7 +20,7 @@ void swim(int speed, std::string name)
         timer++;
     }
     topList.lock();
-    top[timer].push_back(name);
+    top.push_back(name);
     topList.unlock();
 }
 int main()
@@ -35,20 +35,9 @@ int main()
     {
         swimmingLanes[i].join();
     }
-    int place(1);
-    for (std::map<int, std::vector<std::string>>::iterator it = top.begin(); it != top.end(); ++it)
+
+    for (int i = 0; i < top.size(); ++i)
     {
-        std::cout << "Place #"<<place<<": ";
-        if (it->second.size() == 1)
-            std::cout << it->second[0];
-        else
-        {
-            for (int j = 0; j < it->second.size(); j++)
-            {
-                std::cout << it->second[j] << "|";
-            }
-        }
-        std::cout << std::endl;
-        place++;
+        std::cout << "Place #" << i + 1 << ": " << top[i] << std::endl;
     }
 }
